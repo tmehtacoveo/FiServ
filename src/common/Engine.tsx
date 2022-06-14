93c92c18ef655625dfc62a98e806a5e8732bd624
Muhammad Hamza Sumbal <mhsumbal@coveo.com>
2022-06-14 15:00:11 -0400

    changes for hosting client side only

--- src/common/Engine.tsx
+++ src/common/Engine.tsx
@@ -1,4 +1,9 @@
 import {buildSearchEngine} from '@coveo/headless';
+import {
+  PlatformClient,
+  RestUserIdType,
+  TokenModel,
+} from '@coveord/platform-client';
 
 const getEndpointToLocalServer = () => {
   if (!process.env.REACT_APP_SERVER_PORT) {
@@ -14,8 +19,14 @@ const getTokenEndpoint = () => {
 };
 
 export async function getSearchToken() {
-  const res = await fetch(getTokenEndpoint());
-  const {token} = await res.json();
+
+  
+  /* const res = await fetch(getTokenEndpoint()); */
+  /* const {token} = await res.json(); */
+
+  const res = await ensureClientTokenGenerated();
+  const {token} = res;
+
   return token;
 }
 
@@ -28,4 +39,83 @@ export async function initializeHeadlessEngine() {
       renewAccessToken: getSearchToken,
     },
   });
+}
+
+
+async function ensureClientTokenGenerated() {
+  const platform: PlatformClient = new PlatformClient({
+      /**
+       * The Plaform URL to use.
+       * https://platform.cloud.coveo.com is the default platform host.
+       * However, you can target a different environment by changing the host value.
+       *
+       * Example:
+       * Use "https://platformhipaa.cloud.coveo.com" if you want to target the HIPAA environment.
+       *
+       * You can also target a different region (e.g. https://platform-au.cloud.coveo.com)
+       * See https://docs.coveo.com/en/2976/coveo-solutions/deployment-regions-and-strategies#data-residency
+       */
+      host: process.env.REACT_APP_PLATFORM_URL,
+      /**
+       * The unique identifier of your Coveo organization.
+       * To retrieve your org ID, see https://docs.coveo.com/en/148/manage-an-organization/retrieve-the-organization-id
+       */
+      organizationId: process.env.REACT_APP_ORGANIZATION_ID,
+      /**
+       * An API key with the impersonate privilege in the target organization.
+       * See https://docs.coveo.com/en/1718/manage-an-organization/manage-api-keys#add-an-api-key
+       */
+      accessToken: process.env.REACT_APP_API_KEY!,
+    });
+
+  /*   try{ */
+
+  const response  = await platform.search.createToken({
+      /****** Mandatory parameters ******/
+      /**
+       * The security identities to impersonate when authenticating a query with this search token.
+       * The userIds array should contain at least one security indentity.
+       * See https://docs.coveo.com/en/56/#userids-array-of-restuserid-required
+       */
+      userIds: [
+        {
+          name: process.env.REACT_APP_USER_EMAIL!,
+          provider: 'Email Security Provider',
+          type: RestUserIdType.User,
+        },
+      ],
+
+      /****** Optional parameters ******/
+      /**
+       * The name of the search hub to enforce when authenticating a query with this search token.
+       * The search hub is a descriptive name of the search interface on which the token is to be used.
+       *See https://docs.coveo.com/en/56/#searchhub-string-optional
+
+       * Example:
+       * searchHub: 'supporthub',
+       */
+
+      /**
+       * The filter query expression to apply when authenticating a query with this search token.
+       * See https://docs.coveo.com/en/56/#filter-string-optional
+       *
+       * Example:
+       * filter: 'NOT @source="my secured source"',
+       */
+    })
+   /*  .then((data: TokenModel) => {
+      console.log('sdfsdf',data.token)
+      token =  data.token;
+    })
+    .catch((err) => {
+     console.log('error in token ',err)
+     token = null
+    }); */
+      return response;
+    
+ /*  } */
+  /* catch(err){
+    console.log('error in generating search token')
+    return 'error'
+  } */
 }
